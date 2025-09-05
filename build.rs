2324148b93350f5586e7520baa16c750a975b089
aarol <aarol@iki.fi>
2025-09-05 19:18:28 +0300

    Fix full battery icon not loading

--- build.rs
+++ build.rs
@@ -5,7 +5,7 @@ fn main() {
     res.set_icon("src/bat/main.ico");
 
     // register light mode icons (10,20,...,50)
-    for i in (10..50).step_by(10) {
+    for i in (10..=50).step_by(10) {
         res.set_icon_with_id(&format!("src/bat/battery{i}.ico"), &format!("{i}"));
         let charging_i = i + 1;
         res.set_icon_with_id(
@@ -14,7 +14,7 @@ fn main() {
         );
     }
 
-    for i in (15..55).step_by(10) {
+    for i in (15..=55).step_by(10) {
         res.set_icon_with_id(&format!("src/bat/battery{i}.ico"), &format!("{i}"));
         let charging_i = i + 1;
         res.set_icon_with_id(
